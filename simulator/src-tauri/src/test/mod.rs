df2f7b69b0e72cd0ef1c1a2214c182b1a32d8dd4
Seung Ho Jang <shjang1992@gmail.com>
2022-09-15 09:37:01 +0900

    Test fixed

--- simulator/src-tauri/src/test/mod.rs
+++ simulator/src-tauri/src/test/mod.rs
@@ -1,8 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use std::{collections::HashMap, sync::Mutex};
-
-    use tauri::StateManager;
+    use std::collections::HashMap;
 
     use crate::{calculate_skillset, create_data_manager};
 
@@ -15,7 +13,10 @@ mod tests {
         );
 
         let mut selected_skills = HashMap::<String, i32>::new();
-        let free_slots = vec![1, 1, 0, 0];
+        let free_slots = vec![0, 0, 0, 0];
+
+        selected_skills.insert("constitution".to_string(), 2);
+        selected_skills.insert("stamina_surge".to_string(), 2);
 
         calculate_skillset(vec![3, 0, 0], selected_skills, free_slots, &dm);
 
