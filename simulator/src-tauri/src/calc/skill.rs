a32ccc7f41c603efc74b7db8d1e86109428e4449
Seung Ho Jang <shjang1992@gmail.com>
2022-09-19 18:26:42 +0900

    Compile error fix

--- simulator/src-tauri/src/calc/skill.rs
+++ simulator/src-tauri/src/calc/skill.rs
@@ -2,15 +2,15 @@ use crate::data::{deco::Decoration, skill::Skill};
 
 pub struct CalcSkill<'a> {
     base: &'a Skill,
-    decos: &'a Vec<Decoration>,
+    decos: Option<Vec<&'a Decoration>>,
 }
 
 impl<'a> CalcSkill<'a> {
-    pub fn new(base: &Skill, decos: Option<Vec<&Decoration>>) -> Self {
+    pub fn new(base: &'a Skill, decos: Option<Vec<&'a Decoration>>) -> Self {
         Self { base, decos }
     }
 
-    pub fn calculate_point(skill: &Skill, count: i32, decos: &Vec<Decoration>) -> i32 {
+    pub fn calculate_point(skill: &Skill, count: i32, decos: Option<Vec<&Decoration>>) -> i32 {
         let mut point = 0;
 
         if decos.is_none() {
