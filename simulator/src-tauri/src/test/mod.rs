791863e82cab0e1214eaaa42c140538410f271b8
Seung Ho Jang <shjang1992@gmail.com>
2022-09-20 10:41:03 +0900

    Test modified

--- simulator/src-tauri/src/test/mod.rs
+++ simulator/src-tauri/src/test/mod.rs
@@ -13,13 +13,23 @@ mod tests {
         );
 
         let mut selected_skills = HashMap::<String, i32>::new();
+        let weapon_slots = vec![3, 0, 0];
         let free_slots = vec![0, 0, 0, 0];
 
-        selected_skills.insert("constitution".to_string(), 2);
-        selected_skills.insert("stamina_surge".to_string(), 2);
+        selected_skills.insert("water_attack".to_string(), 5);
+        selected_skills.insert("element_exploint".to_string(), 1);
+
+        selected_skills.insert("bow_charge_plus".to_string(), 1);
+        selected_skills.insert("spread_shot".to_string(), 3);
+
+        selected_skills.insert("critical_exploit".to_string(), 3);
+        selected_skills.insert("chain_crit".to_string(), 1);
+
+        selected_skills.insert("constitution".to_string(), 5);
+        selected_skills.insert("stamina_surge".to_string(), 3);
 
         calculate_skillset(
-            vec![3, 0, 0],
+            weapon_slots,
             selected_skills,
             free_slots,
             SexType::Female,
